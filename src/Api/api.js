79e2cd4fa7a5cad6963f9171237f483d99b83ba4
Mohamed Cisse <116883418+McOriginal@users.noreply.github.com>
2025-09-27 12:30:43 +0000

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     process.env.REACT_APP_API_URL ||
-    // 'https://backend-clinique-keneya.onrender.com/api',
-    'http://localhost:5000/api',
+    'https://backend-clinique-keneya.onrender.com/api',
+    // 'http://localhost:5000/api',
 
   headers: {
     'Content-Type': 'application/json',
